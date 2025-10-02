c02631e7015ae79a5fb7bfe7727544918fa93335
Matheus Rocha <matheusburanello21@icloud.com>
2025-10-01 21:22:36 -0300

    Delete aircnc-mobile-dev directory

--- aircnc-mobile-dev/App.tsx
+++
@@ -1,9 +0,0 @@
-import { StatusBar } from 'expo-status-bar';
-import Routes from './src/routes';
-
-export default function App() {
-    return(
-      <Routes />
-    )
-}
-
--- aircnc-mobile-dev/src/services/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL:'http://10.53.52.44:3335',
-})
-
-export default api;
